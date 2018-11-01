9f3559428c54ef5bd04a943c410af52ec01d2fec
eddie <admin@edwinek.uk>
2018-11-01 19:30:15 +0000

    Using image from Dockerhub.

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:8
-ADD get-deps.tar /src
-RUN /src/gradlew clean build -p /src \
-    && rm -rf /src
--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@ APP_NAME=json-rpc-daemon
 CONTAINER_NAME=json_rpc_container
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
-docker build -t json_rpc_image .
-docker run --rm --name $CONTAINER_NAME -tid  -p 8080:8080 -v "$(pwd)":/src json_rpc_image bash
+docker run --rm --name $CONTAINER_NAME -tid  -p 8080:8080 -v "$(pwd)":/src edwinek/openjdk-gradle-springboot:1.0.0 bash
 docker exec -ti $CONTAINER_NAME bash -c "/src/gradlew clean bootJar -p /src \
                                             && ln -s /opt/$APP_NAME/$APP_NAME.jar /etc/init.d/$APP_NAME \
                                             && chmod 755 /etc/init.d/$APP_NAME"
